8bfb1829da66928703ad141e319301264f2edf7b
Katharine Craven <katharine.craven1@marist.edu>
2015-03-07 22:11:10 -0500

    Lab 5 Question 1

---
+++ Lab5.sql
@@ -0,0 +1,11 @@
+﻿--Katharine Craven
+--3/7/15
+--Lab 5
+
+--Question 1
+--Show the cities of agents booking an order for a customer whose cid is 'c006'.
+
+select distinct city from agents inner join orders 
+on (agents.aid = orders.aid)
+where cid = 'c006';
+
