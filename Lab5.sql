75100e89061985d5bf5550ea52a98692f26e26be
Katharine Craven <katharine.craven1@marist.edu>
2015-03-07 22:41:11 -0500

    why
    Subqueries are better.

--- Lab5.sql
+++ Lab5.sql
@@ -9,3 +9,9 @@ select distinct city from agents inner join orders
 on (agents.aid = orders.aid)
 where cid = 'c006';
 
+--Question 2
+--Show the pids of products ordered through any agent who makes at least one order
+--for a customer in Kyoto, sorted by pid from highest to lowest 
+
+(select aid, pid, city from orders inner join customers
+on (orders.cid = customers.cid));
