196ea5b25791cbd42b949e69f6132d080004b264
Katharine Craven <katharine.craven1@marist.edu>
2015-03-13 03:05:07 -0400

    Lab 6 is apparently due tomorrow

---
+++ Lab6.sql
@@ -0,0 +1,32 @@
+﻿--Katharine Craven
+--Lab 6
+--3/13/15
+
+--Question 1
+--Display the name and city of
+-- customers who live in any city
+--that makes the most different kinds of products
+
+select customers.name, customers.city from customers inner join (
+select city from (select max(count) from(
+      select products.city,
+      count(products.pid)
+      from products
+      group by products.city
+      order by count(products.pid)
+) minny) maxie inner join (
+select products.city,
+      count(products.pid)
+      from products
+      group by products.city
+      order by count(products.pid)
+) minny
+on max = count) maxcity
+on customers.city = maxcity.city;
+
+--Question 2
+--Display the names of products whopse priceUSD is below the average
+--in alphabetical order
+
+select name from products 
+where priceUSD < (Select avg(priceUSD) from products);
