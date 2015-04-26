54c834285ae7e78b2a047993dc345109ecc9c27b
Katharine Craven <katharine.craven1@marist.edu>
2015-04-26 17:29:59 -0400

    Lab 10 part 1

---
+++ Lab10.sql
@@ -0,0 +1,31 @@
+﻿----------------------------------------------------------------------------------------
+-- Katharine Craven
+-- Databases Lab 10
+-- 4/26/15
+----------------------------------------------------------------------------------------
+
+-- Part 1
+
+create or replace function PreReqsFor(int) returns refcursor as
+$$
+declare
+   course_Num int :=$1;
+   resultset refcursor = 'prereqsforresult';
+begin
+  open resultset for 
+     select prereqnum
+     from Prerequisites
+       where coursenum = course_Num;
+  return resultset;
+end;
+$$
+language plpgsql;
+
+select PreReqsFor(308);
+Fetch all from prereqsforresult;
+
+select PreReqsFor(499);
+Fetch all from prereqsforresult;
+
+select PreReqsFor(221);
+Fetch all from prereqsforresult;
